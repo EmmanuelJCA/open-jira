fa5867f686e101e3960b002ef93c6683a79de393
EmmanuelJCA <emmanuelcanate@gmail.com>
2023-08-09 21:34:43 -0400

    Entries api get and post

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,7 +1,8 @@
-import { FC, PropsWithChildren, useReducer } from 'react'
+import { FC, PropsWithChildren, useEffect, useReducer } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import { EntriesContext, entriesReducer } from './'
 import { Entry } from '@/interfaces'
+import { entriesApi } from '@/api'
 
 export interface EntriesState {
   entries: Entry[]
@@ -30,6 +31,16 @@ export const EntriesProvider:FC<PropsWithChildren> = ({ children }) => {
   const updateEntry = (entry: Entry) => {
     dispatch({ type: '[Entry] Entry-Updated', payload: entry })
   }
+
+  const refreshEntries = async() => {
+    const { data } = await entriesApi.get<Entry[]>('/entries')
+    dispatch({ type: '[Entry] Refresh-Data', payload: data })
+  }
+
+  useEffect(() => {
+    refreshEntries()
+  }, [])
+  
 
   return (
     <EntriesContext.Provider value={{
